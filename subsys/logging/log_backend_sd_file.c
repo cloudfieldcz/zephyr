a7ab5d6ec34e80480e878393a39442575930f7d0
Martin Rod <martin.rod@cloudfield.cz>
2020-07-21 17:40:31 +0200

    Funkcni, fs_sync() po 100B

--- subsys/logging/log_backend_sd_file.c
+++ subsys/logging/log_backend_sd_file.c
@@ -7,39 +7,53 @@
 #include <drivers/uart.h>
 #include <assert.h>
 
-
 #include <fcntl.h>
 #include <sys/stat.h>
 
-int ecb_open(const char *filename, int mode);
-int ecb_write(int fd, const void *buf, int nbytes);
+#include <fs/fs.h>
 
-const u8_t log_file_name[] = "/SD:/L_LATEST.TXT";
+static const u8_t log_file_name[] = "/SD:/L_LATEST.TXT";
+static struct fs_file_t log_file_fd;
 
-	struct sd_file_device_t {
+struct sd_file_device_t {
 	const u8_t *log_file_name;
-	int fd;
+	struct fs_file_t *log_file_fd_p;
+	bool log_file_open;
+	size_t log_writen;
 };
 
-static struct sd_file_device_t sd_file_device = { .fd =  -1 ,
-						  .log_file_name = 
-							  log_file_name  };
+static struct sd_file_device_t sd_file_device = { .log_file_name =
+							  log_file_name,
+						  .log_file_fd_p = &log_file_fd,
+						  .log_file_open = false,
+						  .log_writen = 0 };
 
 static int sd_file_char_out(u8_t *data, size_t length, void *ctx)
 {
 	struct sd_file_device_t *dev = (struct sd_file_device_t *)ctx;
 
-	if (dev->fd < 0) {
+	if (dev->log_file_open == false) {
 		return 0;
 	}
 
-	int ret = ecb_write(dev->fd, data, length);
-
-	if (ret<0) {
+	int ret = fs_write(dev->log_file_fd_p, data, length);
+	if (ret < 0) {
 		return 0;
+	}
+
+	if (dev->log_writen > 100) {
+		int ret2 = fs_sync(dev->log_file_fd_p);
+		if (ret2 != 0) {
+			return 0;
+		}
+		dev->log_writen=0;
+	}
+	else {
+		dev->log_writen++;
 	}
 
 	return ret;
+	//	return length;
 }
 
 static u8_t buf;
@@ -48,25 +62,19 @@ LOG_OUTPUT_DEFINE(log_output, sd_file_char_out, &buf, 1);
 
 static void put(const struct log_backend *const backend, struct log_msg *msg)
 {
-	u32_t flag = IS_ENABLED(CONFIG_LOG_BACKEND_UART_SYST_ENABLE) ?
-			     LOG_OUTPUT_FLAG_FORMAT_SYST :
-			     0;
-
+	u32_t flag = 0;
 	log_backend_std_put(&log_output, flag, msg);
 }
 
 static void log_backend_sd_file_init(void)
 {
 	struct sd_file_device_t *dev = &sd_file_device;
+	volatile int res = fs_open(dev->log_file_fd_p, dev->log_file_name);
 
-	int fd = ecb_open(dev->log_file_name, O_WRONLY | O_CREAT | O_BINARY);
-	if (fd >= 0) {
-		//LOG_DBG("Log file open OK log_file_name:%s", log_file_name);
-		dev->fd = fd;
+	if (res == 0) {
+		dev->log_file_open = true;
 	} else {
-		//LOG_ERR("Log file open ERR log_file_name:%s fd:%d",
-		//	log_file_name, fd);
-		dev->fd = -1;
+		dev->log_file_open = false;
 	}
 
 	log_output_ctx_set(&log_output, dev);
@@ -80,7 +88,6 @@ static void panic(struct log_backend const *const backend)
 static void dropped(const struct log_backend *const backend, u32_t cnt)
 {
 	ARG_UNUSED(backend);
-
 	log_backend_std_dropped(&log_output, cnt);
 }
 
@@ -88,9 +95,7 @@ static void sync_string(const struct log_backend *const backend,
 			struct log_msg_ids src_level, u32_t timestamp,
 			const char *fmt, va_list ap)
 {
-	u32_t flag = IS_ENABLED(CONFIG_LOG_BACKEND_UART_SYST_ENABLE) ?
-			     LOG_OUTPUT_FLAG_FORMAT_SYST :
-			     0;
+	u32_t flag = 0;
 
 	log_backend_std_sync_string(&log_output, flag, src_level, timestamp,
 				    fmt, ap);
@@ -100,9 +105,7 @@ static void sync_hexdump(const struct log_backend *const backend,
 			 struct log_msg_ids src_level, u32_t timestamp,
 			 const char *metadata, const u8_t *data, u32_t length)
 {
-	u32_t flag = IS_ENABLED(CONFIG_LOG_BACKEND_UART_SYST_ENABLE) ?
-			     LOG_OUTPUT_FLAG_FORMAT_SYST :
-			     0;
+	u32_t flag = 0;
 
 	log_backend_std_sync_hexdump(&log_output, flag, src_level, timestamp,
 				     metadata, data, length);
